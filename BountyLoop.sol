5916fb47f5b826b3f6793d9cb27fde64540abf08
Kamil <kamil.bandzwolek@gmail.com>
2023-06-02 21:38:10 +0200

    Wersja 2
    dla 1 rachunku

--- BountyLoop.sol
+++ BountyLoop.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Charity {
+contract Bountyloop is Ownable {
     using Counters for Counters.Counter;
 
-    event CampaignStarted(bytes32 campaignId, address initiator);
-    event WithdrawFunds(bytes32 campaignId, address initiator, uint256 amount);
-    event FundsDonated(bytes32 campaignId, address donor, uint256 amount);
+    event BountyStarted(bytes32 bountyId, address account);
+    event WithdrawFunds(bytes32 bountyId, address charity, uint256 amount);
+    event BountyDonated(bytes32 bountyId, address donor, uint256 amount);
 
     Counters.Counter public _campaignCount;
 
@@ -19,105 +19,77 @@ contract Charity {
         string description;
         uint256 fundsRaised;
         bool isLive;
-        address initiator;
-        uint256 deadline;
+        address account;
         uint256 balance;
     }
 
-    mapping(bytes32=>Campaign) public _campaigns;
-    mapping(address=>mapping(bytes32=>uint256)) public userCampaignDonations;
+    mapping(bytes32 => Campaign) public _campaigns;
+    mapping(address => mapping(bytes32 => uint256)) public userCampaignDonations;
 
-    constructor(){
+    constructor() {}
 
-    }
+    function startCampaign(
+        address account,
+        string calldata title,
+        string calldata description,
+        string calldata imgUrl,
+        uint256 deadline
+    ) public onlyOwner {
+        bytes32 bountyId = generateBountyId(account, title, description);
 
-    function generateCampaignId(address initiator, string calldata title, string calldata description) public pure returns(bytes32) {
-       bytes32 campaignId = keccak256(abi.encodePacked(title, description, initiator));
-       return campaignId;
-    }
-
-    function startCampaign( string calldata title, string calldata description, string calldata imgUrl, uint256 deadline ) public {
-        // generate a campaignID 
-        // using the title, description and the address of the initiator
-        bytes32 campaignId = generateCampaignId(msg.sender, title, description);
-
-        // get a reference to the campaign with the generated Id
-        Campaign storage campaign = _campaigns[campaignId];
-        // require that the campaign is not live yet.
-        require(!campaign.isLive, "Campaign exists");
-        // require the current time to be less than the campaign deadline
-        require(block.timestamp < deadline, "Campaign ended");
+        Campaign storage campaign = _campaigns[bountyId];
+        require(!campaign.isLive, "Bounty campaign exists");
+        require(block.timestamp < deadline, "Bounty campaign ended");
 
         campaign.title = title;
         campaign.description = description;
-        campaign.initiator = msg.sender;
+        campaign.account = account;
         campaign.imgUrl = imgUrl;
-        campaign.deadline = deadline;
         campaign.isLive = true;
 
-        // increment the total number of charity campaigns created
         _campaignCount.increment();
 
-        // emit an event to the blockchain
-        emit CampaignStarted(campaignId, msg.sender);
+        emit BountyStarted(bountyId, account);
     }
 
-    function endCampaign() public {
-
+    function generateBountyId(address account, string calldata title, string calldata description)
+        public
+        pure
+        returns (bytes32)
+    {
+        bytes32 bountyId = keccak256(abi.encodePacked(account, title, description));
+        return bountyId;
     }
 
-    // allows users to donate to a charity campaign of their choice
-    function donateToCampaign(bytes32 campaignId) public payable {
-        // get campaign details with the given campaign
-        Campaign storage campaign = _campaigns[campaignId];
+    function stopCampaign(bytes32 bountyId) public onlyOwner {
+        Campaign storage campaign = _campaigns[bountyId];
+        require(campaign.isLive, "Campaign is not live");
 
-        // end the campaign if the deadline is exceeded
-        if(block.timestamp > campaign.deadline){
-            campaign.isLive = false;
-        }
-        // require the campaign has not ended
-        require(block.timestamp < campaign.deadline, "Campaign has ended");
+        campaign.isLive = !campaign.isLive;
+    }
+
+    function donateToCampaign(bytes32 bountyId) public payable {
+        Campaign storage campaign = _campaigns[bountyId];
+
+        require(msg.value > 0, "Wrong ETH value");
 
         uint256 amountToDonate = msg.value;
-        require(amountToDonate > 0, "Wrong ETH value");
-
-        // increase the campaign balance by the amount donated;
         campaign.fundsRaised += amountToDonate;
         campaign.balance += amountToDonate;
 
-        // keep track of users donation history
-        userCampaignDonations[msg.sender][campaignId] = amountToDonate;
+        userCampaignDonations[msg.sender][bountyId] = amountToDonate;
 
-        // emit FundsDonated event
-        emit FundsDonated(campaignId, msg.sender, amountToDonate);
+        emit BountyDonated(bountyId, msg.sender, amountToDonate);
     }
 
-    // returns the details of a campaign given the campaignId
-    function getCampaign(bytes32 campaignId) public view returns(Campaign memory) {
-        return _campaigns[campaignId];
+    function getCampaign(bytes32 bountyId) public view returns (Campaign memory) {
+        return _campaigns[bountyId];
     }
 
-    function withdrawCampaignFunds(bytes32 campaignId) public {
-        Campaign storage campaign = _campaigns[campaignId];
-
-        // require the msg.sender is the creator of the campaign
-        require(msg.sender == campaign.initiator, "Not campaign initiator");
-        // require the campaign has ended
-        require(!campaign.isLive, "campaign is still active");
-        require(block.timestamp > campaign.deadline, "Campaign is still active");
-        // require the campaign has funds to be withdrawn
+    function withdrawCampaignFunds(bytes32 bountyId) public {
+        Campaign storage campaign = _campaigns[bountyId];
         require(campaign.balance > 0, "No funds to withdraw");
-
-        uint256 amountToWithdraw = campaign.balance;
-
-        // zero the campaign balance
-        campaign.balance = 0;
-
-        // transfer the balance to the initiator address;
-        payable(campaign.initiator).transfer(amountToWithdraw);
-
-        // emit an event to the blockchain
-        emit WithdrawFunds(campaignId, campaign.initiator, amountToWithdraw);
-    }
-
+                payable(campaign.account).transfer(campaign.balance);
+                emit WithdrawFunds(bountyId, campaign.account, campaign.balance);
+    }   
 }
